9ac8a3a6e0a9dad93ffb1bce7d4d6671e7bd5950
Farhan Rashid <Far20shid@gmail.com>
2018-04-29 11:22:15 +0600

    fixed addplaces

--- login/addplaceserver.php
+++ login/addplaceserver.php
@@ -16,7 +16,8 @@ $a = $_POST['address'];
  $ar=explode("/",$_FILES['fileToUpload']['type']);
 
  print_r($GLOBALS);
- if(empty($u)||empty($d)||empty($t)||empty($c)||empty($sg)||empty($a)){
+ if(empty($u)||empty($d)||empty($t)||empty($c)||empty($sg)||empty($a))
+ {
 echo "return back you cannot place any field empty";
 
  }
@@ -27,54 +28,45 @@ echo "return back you cannot place any field empty";
      //when we find it empty
  $sql = "update tourist_spot SET name= '$u' , type ='$t', description ='$d',country= '$c', address = '$a', suggested_season ='$sg' , imageloc='$img'  where spot_id = '$uid'";
  echo $sql;
- if(updateSQL($sql)){
+ if(updateSQL($sql))
+ {
  echo "data has been updated";
 
  }
- else{
-   echo "DB Error!";
+ else {
+
+   echo "db error!";
  }
+
+} else {
+if ($ar[0]!="image"){
+  echo "filetype not supported";
+}
+else {
+$sql2 = "update tourist_spot SET name = '$u',type='$t' , description='$d', country='$c', address='$a', suggested_season='$sg',imageloc= 'uploads/".$n."'   where spot_id='$uid'";
+echo $sql2;
+
+if(move_uploaded_file($s,"uploads/".$n) ){
+if (updateSQL($sql2) ){
+
+echo "Data has been saved";
+
+} else {
+  echo " DB Error!";
+}
+}  else {
+echo   "file upload error";
+}
+
 }
 }
 
 
 
-//  else {
-//    if($ar[0]!="image"){
-// echo "filetype not supported";
-//    }
-//    else{
-//   $sql = "update tourist_spot SET name= '$u' , type ='$t', description ='$d',country= '$c', address = '$a', suggested_season ='$sg' , imageloc='uploads/.$n'  where spot_id = '$uid'"; //change this
-// if(move_uploaded_file($s,"uploads/".$n)){
-//   echo $sql;
-//   if(updateSQL($sql)){
-//   echo "data has been updated";
-//
-//   }
-//   else{
-//     echo "DB Error!";
-//   }
-// }
-// else {
-// echo "image not found ";
-//
-// }
-// }
-//
-//
-// }
 
 
 
 
-//   $u = $_POST['name'];
-//     $d = $_POST['des'];
-//
-//   $t = $_POST['type'];
-//   $a = $_POST['address'];
-//    $c =$_POST['country'];
-//    $sg = $_POST['sugg'];
-//    $img = $_POST['img'];
-//
-// $sql2 = "insert into tourist_spot values (    )";
-?>
+}
+
+} ?>
